0d55e9011a6cfe8cd10a8c7f91db9956a379619d
Marcos Camara <marcos.tc@gmail.com>
2020-09-21 16:44:01 -0300

    make js work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,12 +10,4 @@ require("channels")
 
 // app/javascript/packs/application.js
 import 'bootstrap';
-import improveDropdown from '../components/select';
 
-improveDropdown();
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
